117dcd92c8135b4cf4ab99eb68d96888ac919407
aknobloch <aaronknobloch92@gmail.com>
2017-10-14 21:51:48 -0400

    Add ExecuteAction class

---
+++ BTConnectionService/BTConnectionService/control/ExecuteAction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BTConnectionService.control
+{
+    class ExecuteAction
+    {
+        public static void Copy()
+        {
+            SendKeys.SendWait("^c");
+        }
+
+        public static void Paste()
+        {
+            SendKeys.SendWait("^v");
+        }
+
+        public static void SelectAll()
+        {
+            SendKeys.SendWait("^a");
+        }
+
+        public static void AltTab()
+        {
+            SendKeys.SendWait("%({TAB})");
+        }
+    }
+}
